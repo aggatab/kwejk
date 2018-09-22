7578692b76bef3f0bb6f9fa41eb486139d904c7e
kasiarzynka <kasiade2@gmail.com>
2018-09-22 15:22:04 +0200

    gif

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -1,12 +1,22 @@
 package pl.akademiakodu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.akademiakodu.model.Gif;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping ("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif(true,"compiler-bot", "kasia");
+        modelMap.put("gif", gif);
+        return "gif-details";
+
     }
 }
