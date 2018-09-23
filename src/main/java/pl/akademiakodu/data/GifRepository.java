70cee6d5d07be4d0179c099b2ad2c7813d87ab99
kasiarzynka <kasiade2@gmail.com>
2018-09-23 11:27:25 +0200

    poprawka

---
+++ src/main/java/pl/akademiakodu/data/GifRepository.java
@@ -0,0 +1,57 @@
+package pl.akademiakodu.data;
+
+import org.springframework.stereotype.Component;
+import pl.akademiakodu.model.Gif;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class GifRepository {
+
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif(true, "android-explosion", "Agata"),
+            new Gif(true, "ben-and-mike", "Ola"),
+            new Gif(false, "compiler-bot", "Zosia"),
+            new Gif(true, "infinite-andrew", "Karol")
+    );
+
+    public Gif findByName(String name) {
+        for (Gif gif : ALL_GIFS) {
+            if (gif.getName().equals(name)) {
+                return gif;
+            }
+        }
+        return null;
+    }
+
+    public List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
+
+    public List<Gif> getFavorites() {
+        for (Gif gif : ALL_GIFS) {
+            if (gif.isFavorite()) {
+                return new ArrayList<>(ALL_GIFS);
+            }
+        }
+        return ALL_GIFS;
+    }
+}
+//
+//    public List<Gif> getFavorites() {
+//        List<Gif> favoritesGifs = new ArrayList<>();
+//
+//        for (Gif gif : ALL_GIFS) {
+//            if (gif.getFavorite()) {
+//                favoritesGifs.add(gif);
+//            }
+//        }
+//        return favoritesGifs;
+//    }
+
+
+
